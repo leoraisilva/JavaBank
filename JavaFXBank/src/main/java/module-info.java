1b5ded5a85d31bdbc808c13613060ee06e9040ac
leoraisilva <lraimundo409@gmail.com>
2023-08-25 23:11:38 -0300

    Inicializando

---
+++ JavaFXBank/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.bank.javafxbank {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.bank.javafxbank to javafx.fxml;
+    exports com.bank.javafxbank;
+}
